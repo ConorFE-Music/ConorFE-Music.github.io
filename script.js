c12dffaa798a48f74f3620a80495a182cc886f66
ConorFE-Music <animeccount123@gmail.com>
2026-09-13 14:52:02 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+script.js
+/* =========================================================
+   MUSIC PORTFOLIO
+   JAVASCRIPT FILE
+
+   At the moment, the portfolio doesn't need much
+   JavaScript because:
+
+   - The music players use HTML
+   - The artist statement dropdowns use HTML
+   - The layout uses CSS
+
+   We can add custom interactions here later.
+========================================================= */
+
+
+/*
+    FUTURE IDEAS:
+
+    - Custom music player
+    - Animated album covers
+    - Page transitions
+    - Scroll animations
+    - Visual effects
+    - Custom navigation
+*/
+```
